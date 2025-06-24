6e03d2fe6b6dbd283652c7d4ef0411d738073d71
istom24 <163309165+istom24@users.noreply.github.com>
2025-06-24 05:25:08 +0300

    Update user.py

--- user.py
+++ user.py
@@ -4,6 +4,8 @@ from .decorators import error_decorator
 from datetime import datetime, timedelta
 from .decorators import db_connection_decorator
 
+
+
 class User(Shape):
     @error_decorator
     def import_from_csv(self, filepath: str):
